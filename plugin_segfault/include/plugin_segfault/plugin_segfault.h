e2e60e4cdbdd883465e34ac66569835181d5f8ab
abhinaba.habishyashi <abhinaba.habishyashi@gmail.com>
2026-04-20 23:31:27 +0200

    Segfault plugin added as requested

---
+++ plugin_segfault/include/plugin_segfault/plugin_segfault.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifdef _WIN32
+    #ifdef PLUGIN_SEGFAULT_BUILDING
+        #define PLUGIN_SEGFAULT_API __declspec(dllexport)
+    #else
+        #define PLUGIN_SEGFAULT_API __declspec(dllimport)
+    #endif
+#else
+    #define PLUGIN_SEGFAULT_API __attribute__((visibility("default")))
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+PLUGIN_SEGFAULT_API int plugin_init(void);
+
+PLUGIN_SEGFAULT_API const char* plugin_get_name(void);
+
+PLUGIN_SEGFAULT_API void plugin_trigger_segfault(void);
+
+PLUGIN_SEGFAULT_API const char* plugin_get_crash_log_path(void);
+
+#ifdef __cplusplus
+}
+#endif
