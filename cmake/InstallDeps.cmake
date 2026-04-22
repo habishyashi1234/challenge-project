07339291257f50442f4fbd928228e8a5f4c06bff
abhinaba.habishyashi <abhinaba.habishyashi@gmail.com>
2026-04-23 00:29:52 +0200

    Added the Task 4 installation step for the project

---
+++ cmake/InstallDeps.cmake
@@ -0,0 +1,27 @@
+include(GNUInstallDirs)
+
+if(WIN32)
+    set(_lib_pattern "*.dll")
+    set(_dest "${CMAKE_INSTALL_BINDIR}")
+else()
+    set(_lib_pattern "*.so*")
+    set(_dest "${CMAKE_INSTALL_LIBDIR}")
+endif()
+
+# Conan 2 CMakeDeps sets boost_LIB_DIRS_<CONFIG> after find_package(Boost)
+string(TOUPPER "${CMAKE_BUILD_TYPE}" _build_type_upper)
+set(_boost_lib_dir "${boost_LIB_DIRS_${_build_type_upper}}")
+
+if(_boost_lib_dir AND EXISTS "${_boost_lib_dir}")
+    file(GLOB _boost_libs "${_boost_lib_dir}/${_lib_pattern}")
+    if(_boost_libs)
+        install(FILES ${_boost_libs}
+            DESTINATION ${_dest}
+            COMPONENT Runtime
+        )
+    else()
+        message(WARNING "No Boost libraries found in ${_boost_lib_dir}")
+    endif()
+else()
+    message(WARNING "boost_LIB_DIRS_${_build_type_upper} not set or does not exist")
+endif()
